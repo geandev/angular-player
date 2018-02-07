a7085dac86567623e1c05c277f3f2c0a03eaa133
Gean Alexandre da Silva <gean.silva@programmers.com.br>
2018-02-07 14:55:26 -0200

    wp

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -29,11 +29,6 @@ namespace Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
             app.UseMvc();
         }
     }
--- Web/src/app/app.component.ts
+++ Web/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Howl, Howler } from 'howler';
+import { sample } from 'rxjs/operator/sample';
 
 @Component({
   selector: 'my-app',
@@ -7,13 +8,29 @@ import { Howl, Howler } from 'howler';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  private sound: Howl
+  private music: string
+  private currentPlaying: number
+
   constructor() {
-    debugger
-    const sound = new Howl({
-      src: ['http://localhost:5000/api/songs/froid.wav'],
+    this.music = "froid.wav"
+    this.sound = new Howl({
+      src: [`http://localhost:5000/api/songs/${this.music}`],
       html5: true,
       format: ['wav']
     })
-    sound.play();
+    this.sound.on("play", (test) => console.info(test))
+  }
+
+  play() {
+    this.currentPlaying = this.sound.play()
+    this.sound.on("seek", ()seek )
+  }
+
+  next() {
+    this.sound.seek(this.sound.duration(this.currentPlaying))
+  }
+  stop() {
+    this.sound.pause()
   }
 }
