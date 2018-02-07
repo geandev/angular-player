5f22a4201f152bb83cdf398d4ed2b4e926cd285c
Gean Alexandre da Silva <gean.silva@programmers.com.br>
2018-02-07 10:59:44 -0200

    added lib front

--- Web/src/app/app.component.ts
+++ Web/src/app/app.component.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
+import { Howl, Howler } from 'howler';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent { }
+export class AppComponent {
+  constructor() {
+    debugger
+    const sound = new Howl({
+      src: ['http://localhost:5000/api/songs/froid.wav'],
+      html5: true,
+      format: ['wav']
+    })
+    sound.play();
+  }
+}
